d510a8db0a132f9c18c176b9f87d0e700eac2775
codeflames <yerekadonald@gmail.com>
2023-06-07 22:47:11 +0100

    mongodb and validation added

--- index.js
+++ index.js
@@ -1,83 +1,120 @@
 // const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+
+
 
 const app = express()
 
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+
+const Note = require('./models/note')
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World yeah!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id =  Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    // response.statusMessage = `Note with id ${id} not found`
-    response.status(404).send('Note not found')
-    .end()
+  // response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
   }
+  )
 })
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
-app.post('/api/notes', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note.findById(id).then(note => {
+    if (note) {
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
+  }).catch(error => {
+    next(error)
+  })
+})
+// const generateId = () => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
+
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  }).catch(error => {
+    next(error)
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important,
   }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(id, note, { new: true, runValidators: true, context: 'query' }).then(updatedNote => {
+    response.json(updatedNote)
+  }).catch(error => {
+    next(error)
+  })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id =  Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+
+
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note.findByIdAndRemove(id).then(result => {
+    response.status(204).end()
+  }).catch(error => {
+    next(error)
+  })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+
 
 
 
@@ -86,5 +123,3 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// console.log(`Server running at http://localhost:${port}`)
